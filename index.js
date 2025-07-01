feddee02e7cdf7a5da4a1edbca54042d03986213
omme salma <ommes918@gmail.com>
2025-07-01 20:35:48 +0600

    again commit

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // or "*" for any origin
+  credentials: true // if using cookies or auth headers
+}));
 app.use(express.json());
 
 
